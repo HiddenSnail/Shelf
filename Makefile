4322ddc126c5474a92c0e9bebae233e87e4c467c
HiddenSnail <741409033@qq.com>
2017-12-10 14:16:20 +0800

    write a testdb demo and modify makefile

--- Makefile
+++ Makefile
@@ -3,28 +3,40 @@ PKG_SOURCE = ./docs ./include ./src Makefile
 
 SERVER_USR = root
 SERVER_IP = 139.196.94.117
+SERVER_PROJECT_DIR = workspace/Shelf
 
 CXX = g++
 CXX_HEAD_FLAGS = --std=c++11
 CXX_END_FLAGS = `pkg-config --cflags --libs libmongocxx` -Wl,-rpath,/usr/local/lib
 
 shelf : ./src/testdb.cpp
-	$(CXX) $(CXX_HEAD_FLAGS) -o shelf.out ./src/testdb.cpp $(CXX_END_FLAGS)
+	@$(CXX) $(CXX_HEAD_FLAGS) -o shelf.out ./src/testdb.cpp $(CXX_END_FLAGS)
 
-.PHONY : run
-run : 
-	./shelf.out
+
+
 
 .PHONY : pkg
 pkg :
-	-rm $(PKG_NAME).tar
-	tar -cvf $(PKG_NAME).tar $(PKG_SOURCE)
+	@-rm $(PKG_NAME).tar
+	@tar -cf $(PKG_NAME).tar $(PKG_SOURCE)
 
 .PHONY : dpkg
 dpkg:
-	tar -xf $(PKG_NAME).tar
+	@tar -xf $(PKG_NAME).tar
 
 .PHONY : push
 push :
-	scp ./$(PKG_NAME).tar $(SERVER_USR)@$(SERVER_IP):/$(SERVER_USR)/workspace/Shelf
-	scp ./Makefile $(SERVER_USR)@$(SERVER_IP):/$(SERVER_USR)/workspace/Shelf
+	@scp ./$(PKG_NAME).tar $(SERVER_USR)@$(SERVER_IP):/$(SERVER_USR)/$(SERVER_PROJECT_DIR)
+	@scp ./Makefile $(SERVER_USR)@$(SERVER_IP):/$(SERVER_USR)/$(SERVER_PROJECT_DIR)
+
+.PHONY : compile
+compile :
+	@echo compiling source code ...
+	@make pkg
+	@make push
+	@ssh $(SERVER_USR)@$(SERVER_IP) "cd $(SERVER_PROJECT_DIR) && make dpkg && make"
+	@echo compiling finish!
+
+.PHONY : run
+run : 
+	@ssh $(SERVER_USR)@$(SERVER_IP) "cd $(SERVER_PROJECT_DIR) && ./shelf.out"
--- src/testdb.cpp
+++ src/testdb.cpp
@@ -4,20 +4,52 @@
 
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+#include <vector>
+#include <string>
+
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
 
 int main(int, char**) {
     mongocxx::instance inst{};
-    mongocxx::client conn{mongocxx::uri{}};
+    mongocxx::client conn{mongocxx::uri{"mongodb://localhost:27017"}};
+    auto collection = conn["colorbook"]["test_col"];
 
-    bsoncxx::builder::stream::document document{};
-
-    auto collection = conn["mydb"]["mycollection"];
-    document << "Snail" << "Hello";
-
-    collection.insert_one(document.view());
-    auto cursor = collection.find({});
-
-    for (auto&& doc : cursor) {
-        std::cout << bsoncxx::to_json(doc) << std::endl;
+    std::vector<bsoncxx::document::value> docs;
+    for (int i = 0; i < 5; i++) {
+        bsoncxx::builder::stream::document doc{};
+        docs.push_back(doc << "index" << i 
+        << "value" << std::string("value") + std::to_string(i)
+        << finalize);
     }
+    collection.insert_many(docs);
+
+    collection.update_many(
+        document{} << "index" << open_document
+        << "$lte" << 2
+        << close_document << finalize,
+        document{} << "$set" << open_document
+        << "index" << 4
+        << close_document << finalize
+    );
+    
+    mongocxx::cursor cursor = collection.find(
+        document{} << "index" << open_document
+        << "$gt" << 2
+        << "$lte" << 5
+        << close_document
+        << finalize);
+    for (auto data: cursor) {
+        std::cout << bsoncxx::to_json(data) << std::endl;
+    }
+
+    bsoncxx::stdx::optional<mongocxx::result::delete_result> result = collection.delete_many(document{} << finalize);
+    if (result) {
+        std::cout << "delete " << result->deleted_count() << " documents" << std::endl;
+    }
+    return 0;
 }
